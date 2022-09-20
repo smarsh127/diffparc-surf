eb5d40ae8f1a28d723234e2a4e94c9fd9909e53f
Ali Khan <alik@robarts.ca>
2022-09-20 17:49:22 -0400

    make gathering csv files work for older versions of mrtrix

--- diffparc/workflow/scripts/gather_csv_files.py
+++ diffparc/workflow/scripts/gather_csv_files.py
@@ -1,5 +1,6 @@
 from glob import glob
 import os
+import numpy as np
 
 in_dir = snakemake.input.conn_dir
 header_line = snakemake.params.header_line
@@ -9,12 +10,23 @@ out_csv = snakemake.output.conn_csv
 csv_files = sorted(glob(os.path.join(in_dir, "*.csv")))
 all_lines = list()
 
+
+#make this function backwards-compatible -- newer versions of mrtrix use csv and have command_history line
+# older version is space-delimited, no header line
+with open(csv_files[0],"r") as f:
+    first_line = f.readline()
+    print(first_line)
+    if 'command_history' in first_line:
+        load_opts={'delimiter': ',', 'skiprows':1}
+    else:
+        load_opts={}
+
 all_lines.append(header_line + "\n")
 
 for csv in csv_files:
-    with open(csv, "r") as f:
-        skiphdr = f.readline()
-        all_lines.append(f.readline())
+    row_data = np.loadtxt(csv,**load_opts,dtype='int16')
+    all_lines.append(','.join([f'{r}' for r in row_data])+'\n' )
+
 
 with open(out_csv, "w") as f:
     f.writelines(all_lines)
