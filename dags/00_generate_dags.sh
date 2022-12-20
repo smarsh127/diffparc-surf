273b51462a2558cbc2bc82324384f8ada1fa460f
Ali Khan <akhanf@users.noreply.github.com>
2022-12-20 17:31:36 -0500

    add script to generate dags (#38)
    generates a whole bunch of variants for inspecting the workflow

---
+++ dags/00_generate_dags.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+test_data="../diffparc/test_data/bids_AP_PA"
+snakedwi_dir="../diffparc/test_data/snakedwi_dir"
+tmp_dir=`mktemp -d`
+
+
+for dagtype in dag rulegraph filegraph
+do
+
+mkdir -p ${dagtype}
+poetry run diffparc $test_data $tmp_dir/out participant --${dagtype} | dot -Tpdf > ${dagtype}/${dagtype}_default.pdf
+
+poetry run diffparc $test_data $tmp_dir/out participant --${dagtype} --config methods=['mrtrix','fslprobtrack'] surface_metrics=['surfarea','inout','indepconn'] | dot -Tpdf > ${dagtype}/${dagtype}_fsl_mrtrix.pdf
+
+poetry run diffparc $test_data $tmp_dir/out participant --${dagtype} --use-eddy | dot -Tpdf > ${dagtype}/${dagtype}_use_eddy.pdf
+
+poetry run diffparc $test_data $tmp_dir/out participant --${dagtype} --use-topup --use-eddy | dot -Tpdf > ${dagtype}/${dagtype}_use_topup_eddy.pdf
+
+poetry run diffparc $test_data $tmp_dir/out participant --${dagtype} --in-snakedwi-dir $snakedwi_dir | dot -Tpdf > ${dagtype}/${dagtype}_skip_dwi_preproc.pdf
+
+for metric in surfarea inout indepconn FA 
+do
+    poetry run diffparc $test_data $tmp_dir/out participant --${dagtype} --in-snakedwi-dir $snakedwi_dir --config surface_metrics=[\'${metric}\'] | dot -Tpdf > ${dagtype}/${dagtype}_${metric}_skip_dwi_preproc.pdf
+done
+
+
+done
+
+
+
