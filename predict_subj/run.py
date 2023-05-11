ee0dc6165811876260de280ce86cffb611ad0b2a
Ali Khan <alik@robarts.ca>
2023-05-11 17:21:46 -0400

    use deployed S3 models

--- predict_subj/run.py
+++ predict_subj/run.py
@@ -8,7 +8,8 @@ from snakemake import snakemake
 def run_app(args):
     config = {
         'in_archive': args.in_archive,
-        'in_model': args.in_model,
+        's3_bucket_models': args.s3_bucket_models,
+        's3_model_zip': args.s3_model_zip,
         'out_csv': args.out_csv,
         'age': args.age,
         'sex': args.sex,
@@ -23,7 +24,8 @@ def run_app(args):
 def main():
     parser = argparse.ArgumentParser(description='End-to-end diffparc prediction for a single subject, from dicom to classification output')
     parser.add_argument('--in-archive', required=True, help='Input archive file (tar, tgz, zip, tar.gz) containing dicom files for a single subject')
-    parser.add_argument('--in-model', required=True, help='Input photonai model directory')
+    parser.add_argument('--s3-model-zip', required=False, help='Path to input model zip inside S3 bucket')
+    parser.add_argument('--s3-bucket-models', required=False, help='Name of S3 bucket containing photonai models')
     parser.add_argument('--age', required=True, help='Age of subject')
     parser.add_argument('--sex', required=True, help='Sex of subject')
     parser.add_argument('--out-csv', required=True, help='Output csv file')
